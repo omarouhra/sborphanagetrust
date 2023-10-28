959ce03c73982df0d7ea05143286f7dbe85f8dc6
chaimaaSafi <safichaimaa098@gmail.com>
2023-10-28 03:06:09 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="font-semibold text-3xl">Welcome back</h1>
+    </div>
+  );
+};
+
+export default Home;
