b210417707cbc7e4a9c71a424ef6d246599b4773
chaimaaSafi <safichaimaa098@gmail.com>
2023-10-30 19:48:07 +0100

    fix: rename Topbar component to NavBanner

--- src/components/Navbar/ NavBanner.tsx
+++ src/components/Navbar/ NavBanner.tsx
@@ -5,19 +5,19 @@ import FacebookIcon from '@/icons/facebookIcon';
 import PhoneIcon from '@/icons/phoneIcon';
 import YoutubeIcon from '@/icons/youtubeIcon';
 
-function TopBar() {
+function NavBanner() {
   return (
-    <section className="flex flex-col space-y-2 md:flex-row md:items-center w-full md:justify-between md:space-x-3 bg-green-g1 px-10 md:px-40 py-5 text-white">
+    <section className="flex w-full flex-col items-start space-y-2 bg-green-g1 px-3 py-5 text-white md:flex-row md:items-center md:justify-between md:space-x-3 md:px-10 xl:px-40">
       <div className="flex items-center space-x-4">
         <YoutubeIcon />
         <FacebookIcon />
       </div>
-      <div className="flex flex-col space-y-2 md:flex-row md:items-center md:space-x-8 text-sm">
+      <div className="flex flex-col items-start space-y-2 text-sm md:flex-row md:items-center md:space-x-8 md:space-y-0">
         <div className="flex items-center space-x-2">
           <EmailIcon />
           <span>info@sborphanagetrust.co.uk</span>
         </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex space-x-2 md:items-center">
           <PhoneIcon />
           <span>0790 310 6037</span>
         </div>
@@ -26,4 +26,4 @@ function TopBar() {
   );
 }
 
-export default TopBar;
+export default NavBanner;
