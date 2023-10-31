904cec1ee6503b27556ccde4b1d987d4683250a4
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-01 00:22:32 +0100

    feat: add news sections to te main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from 'next';
 
+import AboutSB from '@/components/home/sections/AboutSB';
+import Donation from '@/components/home/sections/Donation';
 import Hero from '@/components/home/sections/Hero';
+import Provision from '@/components/home/sections/Provision';
+import Statics from '@/components/home/sections/Statics';
 import Navbar from '@/components/layouts/Navbar';
 
 const Home: NextPage = () => (
-  <div className="flex h-screen w-full flex-col">
+  <div className="flex  w-full flex-col">
     <Navbar />
     <Hero />
+    <div className="bg-gradient-to-b from-[#cbf1e9d5]  to-[#a5b2af5d]">
+      <AboutSB />
+      <Provision />
+    </div>
+    <Statics />
+    <Donation />
   </div>
 );
 
