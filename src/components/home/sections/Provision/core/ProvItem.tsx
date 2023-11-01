bf2e21b770591168be949260e3520360cbeee155
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-01 23:07:43 +0100

    fix:  ProvItem width in 'md' screen

--- src/components/home/sections/Provision/core/ProvItem.tsx
+++ src/components/home/sections/Provision/core/ProvItem.tsx
@@ -8,14 +8,16 @@ type Props = {
 
 export default function ProvItem({ icon, title, text }: Props) {
   return (
-    <div className="flex h-[270px] w-full flex-col items-center justify-center rounded-3xl bg-white px-2  py-6 md:w-[300px]">
+    <div className="flex w-full flex-col items-center justify-center rounded-3xl bg-white px-2 py-6 md:h-[300px]  md:w-[300px] lg:h-[270px]">
       <div className="mb-4">
         <div className="flex h-20 w-20 items-center justify-center rounded-full bg-green-g1 text-white">
           {icon}
         </div>
       </div>
       <p className="mb-2 text-2xl font-bold text-green-g1 ">{title}</p>
-      <p className="h-16 text-center text-base  text-dark-d1">{text}</p>
+      <p className="text-center text-base text-dark-d1 md:h-32  lg:h-16">
+        {text}
+      </p>
     </div>
   );
 }
