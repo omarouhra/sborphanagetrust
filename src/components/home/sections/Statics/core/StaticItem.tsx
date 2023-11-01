49d89c9043d37d663ad4bde29376e55ecba64b93
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-01 22:26:34 +0100

    fix: staticItem flex alignement

--- src/components/home/sections/Statics/core/StaticItem.tsx
+++ src/components/home/sections/Statics/core/StaticItem.tsx
@@ -8,9 +8,9 @@ type Props = {
 export default function StaticItem({ icon, title, text }: Props) {
   return (
     <div className="flex h-[250px] w-full flex-col items-center justify-center px-4 py-6 text-white md:w-[270px]">
-      <div className="mb-4">{icon}</div>
-      <p className="mb-2 text-base font-bold ">{title}</p>
-      <p className="text-center text-sm">{text}</p>
+      <div className="mb-4 h-32">{icon}</div>
+      <p className="mb-2 text-3xl font-semibold ">{title}</p>
+      <p className="h-24 text-center text-lg">{text}</p>
     </div>
   );
 }
