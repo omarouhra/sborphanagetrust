5d8fb82a38e02d8057b237a42f1dd61d68ea83ac
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-02 00:57:26 +0100

    feat: add icons

---
+++ src/icons/footer/youtubeIcon.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function YoutubeIcon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.1665 16.6668H5.83317C3.33317 16.6668 1.6665 15.0002 1.6665 12.5002V7.50016C1.6665 5.00016 3.33317 3.3335 5.83317 3.3335H14.1665C16.6665 3.3335 18.3332 5.00016 18.3332 7.50016V12.5002C18.3332 15.0002 16.6665 16.6668 14.1665 16.6668Z"
+        stroke="currentColor"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.5 7.91676L11.5833 9.16676C12.3333 9.66676 12.3333 10.4168 11.5833 10.9168L9.5 12.1668C8.66667 12.6668 8 12.2501 8 11.3334V8.83342C8 7.75009 8.66667 7.41676 9.5 7.91676Z"
+        stroke="currentColor"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
