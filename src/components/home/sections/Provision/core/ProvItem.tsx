ec86d16dfb8b4cc144b2d367356a7a67bf845d59
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-01 22:26:04 +0100

    fix: ProvItem alignement

--- src/components/home/sections/Provision/core/ProvItem.tsx
+++ src/components/home/sections/Provision/core/ProvItem.tsx
@@ -8,12 +8,14 @@ type Props = {
 
 export default function ProvItem({ icon, title, text }: Props) {
   return (
-    <div className="flex h-[250px] w-full flex-col items-center justify-center rounded-3xl bg-white px-4 py-6 md:w-[270px]">
-      <div className="mb-4 flex h-20 w-20 items-center justify-center rounded-full bg-green-g1 text-white">
-        {icon}
+    <div className="flex h-[270px] w-full flex-col items-center justify-center rounded-3xl bg-white px-2  py-6 md:w-[300px]">
+      <div className="mb-4">
+        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-green-g1 text-white">
+          {icon}
+        </div>
       </div>
-      <p className="mb-2 text-base font-bold text-green-g1 ">{title}</p>
-      <p className="text-center text-sm text-dark-d1">{text}</p>
+      <p className="mb-2 text-2xl font-bold text-green-g1 ">{title}</p>
+      <p className="h-16 text-center text-base  text-dark-d1">{text}</p>
     </div>
   );
 }
