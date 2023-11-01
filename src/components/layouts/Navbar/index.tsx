408ea4a41db5404c677d59b271635d090cd28c3f
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-01 23:09:55 +0100

    fix: add max width and update hover links style to Navbar

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -18,53 +18,46 @@ export default function Navbar() {
   return (
     <section className="w-full">
       <NavBanner />
-      <header
-        className={cx(
-          'flex w-full items-center justify-between bg-green-g2  md:px-10  py-5 xl:px-40',
-          isOpen ? 'px-0' : 'px-3',
-        )}
-      >
-        <div className="w-44 lg:w-52">
-          {/**
-           * TODO:change Logo to SVG
-           */}
-          <Link href="/" aria-label="Home">
-            <CustomImage alt="sbo-log" src={Logo} isBlurring={false} priority />
-          </Link>
-        </div>
-        <div
+      <div className="bg-green-g2">
+        <header
           className={cx(
-            'block md:hidden',
-            isOpen && 'fixed top-0 right-0 py-2 z-50 ',
+            'flex w-full items-center justify-between  py-5 max-w-[1140px]  mx-auto',
+            isOpen ? 'px-0' : '',
           )}
         >
-          <Hamburger
-            toggled={isOpen}
-            toggle={setOpen}
-            color="#009174"
-            duration={0.5}
-          />
-        </div>
-        <nav
-          className={cx(
-            'w-full md:max-w-xl md:space-x-5 lg:max-w-2xl lg:items-center md:justify-end lg:space-x-10 xl:max-w-4xl xl:space-x-16',
-            isOpen
-              ? 'fixed top-0 py-28 flex h-screen w-full flex-col bg-white px-3 space-y-4'
-              : 'hidden md:flex',
-          )}
-        >
-          {menuItems.map((menu, index) => {
-            return (
-              <MenuItems
-                items={menu}
-                key={index}
-                isOpen={isOpen}
-                pathname={pathname}
+          <div className="w-44 lg:w-52">
+            {/**
+             * TODO:change Logo to SVG
+             */}
+            <Link href="/" aria-label="Home">
+              <CustomImage
+                alt="sbo-log"
+                src={Logo}
+                isBlurring={false}
+                priority
               />
-            );
-          })}
-        </nav>
-      </header>
+            </Link>
+          </div>
+          <div
+            className={cx(
+              'block lg:hidden',
+              isOpen && 'fixed top-0 right-0 py-2 z-50 ',
+            )}
+          >
+            <Hamburger
+              toggled={isOpen}
+              toggle={setOpen}
+              color="#009174"
+              duration={0.5}
+            />
+          </div>
+          <nav className="hidden w-full flex-row justify-end space-x-3 lg:flex">
+            {menuItems.map((menu, index) => {
+              return <MenuItems items={menu} key={index} pathname={pathname} />;
+            })}
+          </nav>
+        </header>
+      </div>
     </section>
   );
 }
