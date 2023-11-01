200987e2be79ffd936937f6e63b08e950ad52636
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-02 00:58:28 +0100

    feat: add menu footer/bank accout constants

---
+++ src/utils/constants/bankAccount.ts
@@ -0,0 +1,6 @@
+export const BANK_ACCOUNT = [
+  'Name: SB Orphanage Trust',
+  'Account No: 10020276',
+  'Sort Code: 60-02-63',
+  'Bank Name: NatWest Bank',
+];
---
+++ src/utils/constants/footerMenu.ts
@@ -0,0 +1,6 @@
+export const FOOTER_MENU = [
+  { name: 'Home', link: '/' },
+  { name: 'About Us', link: '/about-us' },
+  { name: 'Donate', link: '/donate' },
+  { name: 'Privacy Policy', link: '/privacy' },
+];
