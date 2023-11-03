9a21b36de1e4c646241ab21d740bb1c5c85b2b1c
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-03 11:16:41 +0100

    feat: wrap home page by MainLayout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,10 @@ import Donation from '@/components/home/sections/Donation';
 import Hero from '@/components/home/sections/Hero';
 import Provision from '@/components/home/sections/Provision';
 import Statics from '@/components/home/sections/Statics';
-import Footer from '@/components/layouts/Footer';
-import Navbar from '@/components/layouts/Navbar';
+import MainLayout from '@/components/layouts/MainLayout';
 
 const Home: NextPage = () => (
-  <div className="flex  w-full flex-col">
-    <Navbar />
+  <MainLayout>
     <Hero />
     <div className="bg-gradient-to-b from-[#cbf1e9d5]  to-[#a5b2af5d]">
       <AboutSB />
@@ -18,8 +16,7 @@ const Home: NextPage = () => (
     </div>
     <Statics />
     <Donation />
-    <Footer />
-  </div>
+  </MainLayout>
 );
 
 export default Home;
