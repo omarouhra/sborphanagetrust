9442e40958b764e2f7eb547ad10270f146fc2c6f
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-04 01:48:10 +0100

    feat: add DoatioGrid to home section

--- src/components/home/sections/Donation/index.tsx
+++ src/components/home/sections/Donation/index.tsx
@@ -1,58 +1,10 @@
 import React from 'react';
 
+import DonationGrid from '@/components/core/DonationGrid';
 import Heading from '@/components/core/Heading';
 import Paragraph from '@/components/core/Paragraph';
 import Wrapper from '@/components/layouts/Wrapper';
 
-import DonationTable from './core/DonationTable';
-
-const DONATION = [
-  {
-    appearance: 'normal',
-    title: 'Be a Sponsor',
-    text: 'one child for a year',
-    price: '240',
-    features: [
-      '£240 one off',
-      '£20 monthly direct debit',
-      '100% donation used',
-      'Get frequent updates',
-      'Talk to your sponsored child',
-    ],
-    label: 'Sponsor a child',
-    hasSign: true,
-  },
-  {
-    appearance: 'selected',
-    title: 'Zakat Donation',
-    text: 'make your Zakat donation',
-    price: '2.5%',
-    features: [
-      'Yearly one off',
-      'Monthly direct debit',
-      '100% donation used',
-      'Eligible to receive zakat',
-      'Yearly news and updates',
-    ],
-    label: 'Zakat Donation',
-    hasSign: false,
-  },
-  {
-    appearance: 'normal',
-    title: 'Sadaqah Jariyah',
-    text: 'Unique and sustainable projects',
-    price: 'Any',
-    features: [
-      'One off',
-      'Monthly direct debit',
-      '100% donation used',
-      'Used in unique projects',
-      'Yearly news and updates',
-    ],
-    label: 'Sadaqah Donation',
-    hasSign: false,
-  },
-];
 export default function Donation() {
   return (
     <section className="bg-gradient-to-b from-[#cbf1e9d5] to-[#a5b2af5d]">
@@ -68,15 +20,7 @@ export default function Donation() {
             className="max-w-lg text-center text-grey-2"
           />
         </div>
-        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:gap-24">
-          {DONATION.map((donat, index: number) => (
-            <DonationTable
-              key={index}
-              {...donat}
-              appearance={donat.appearance as any}
-            />
-          ))}
-        </div>
+        <DonationGrid />
       </Wrapper>
     </section>
   );
