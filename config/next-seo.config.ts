00fe6b377be47805de37dbe0d65c52f583fedbb5
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-04 01:14:00 +0100

    feat: next config file

---
+++ config/next-seo.config.ts
@@ -0,0 +1,21 @@
+const NextSeo = {
+  defaultTitle: 'SB Orphanage Trust ',
+  titleTemplate: '%s | SB Orphanage Trust',
+  description: 'SB Orphanage Trust',
+
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://sborphanagetrust.co.uk/',
+    site_name: 'sborphanagetrust',
+    images: [
+      {
+        url: 'https://www.obytes.com/assets/obytes_og_image.png',
+        height: 1920,
+        width: 1920,
+      },
+    ],
+  },
+};
+
+export default NextSeo;
