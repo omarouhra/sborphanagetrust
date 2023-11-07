e08997e9a66e6aeee7c85e5be8256d4ba0251b3a
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-07 22:25:56 +0100

    feat: add next seo to donate page

--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -1,15 +1,20 @@
+import { NextSeo } from 'next-seo';
 import React from 'react';
 
 import Donations from '@/components/donate/sections/Donations';
 import Hero from '@/components/donate/sections/Hero';
 import MainLayout from '@/components/layouts/MainLayout';
+import { baseURL } from '@/utils/constants/baseUrl';
 
 function donate() {
   return (
-    <MainLayout>
-      <Hero />
-      <Donations />
-    </MainLayout>
+    <>
+      <NextSeo title="Donate" canonical={`${baseURL}/donate`} />
+      <MainLayout>
+        <Hero />
+        <Donations />
+      </MainLayout>
+    </>
   );
 }
 
