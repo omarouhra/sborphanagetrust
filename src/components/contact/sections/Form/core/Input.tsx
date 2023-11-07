b1848f91d139fa65302f181da2bdb72957c45823
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-07 20:23:35 +0100

    feat: handlig error in Input comp

--- src/components/contact/sections/Form/core/Input.tsx
+++ src/components/contact/sections/Form/core/Input.tsx
@@ -6,19 +6,25 @@ import { cx } from '@/utils/cx';
 type Props = InputHTMLAttributes<HTMLInputElement> & {
   placeholder: string;
   className?: string;
+  error?: any;
 };
 export const Input = forwardRef<HTMLInputElement, Props>(
-  ({ placeholder, className, ...props }, ref) => {
+  ({ placeholder, className, error, ...props }, ref) => {
+    console.log(error);
     return (
-      <input
-        placeholder={placeholder}
-        ref={ref}
-        className={cx(
-          'w-full py-1.5 border-b border-grey-4 focus:outline-none placeholder:text-grey-4 text-sm font-medium',
-          className,
-        )}
-        {...props}
-      />
+      <div>
+        <input
+          placeholder={placeholder}
+          ref={ref}
+          className={cx(
+            'w-full py-1.5 border-b focus:outline-none placeholder:text-grey-4 text-sm font-medium',
+            error ? 'border-red-500' : 'border-grey-4',
+            className,
+          )}
+          {...props}
+        />
+        {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
+      </div>
     );
   },
 );
