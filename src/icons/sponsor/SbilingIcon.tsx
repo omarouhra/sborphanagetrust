7f7fc517be0c51fdb82a759ff9fdd7206481c1d3
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-22 21:08:32 +0100

    feat: add icons

--- src/icons/sponsor/SbilingIcon.tsx
+++ src/icons/sponsor/SbilingIcon.tsx
@@ -11,7 +11,7 @@ export default function SbilingIcon(props: SVGProps<SVGSVGElement>) {
       fill="none"
       {...props}
     >
-      <g clip-path="url(#clip0_112_2483)">
+      <g clipPath="url(#clip0_112_2483)">
         <path
           fillRule="evenodd"
           clipRule="evenodd"
