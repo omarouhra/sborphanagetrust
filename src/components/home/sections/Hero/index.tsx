4420117a4efb06f00de1f70c2351c096dcd1ac52
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-24 23:41:57 +0100

    feat: update hero section

--- src/components/home/sections/Hero/index.tsx
+++ src/components/home/sections/Hero/index.tsx
@@ -3,19 +3,21 @@ import React from 'react';
 import Button from '@/components/core/Button';
 import Heading from '@/components/core/Heading';
 import Paragraph from '@/components/core/Paragraph';
+import HeroLayout from '@/components/layouts/HeroLayout';
 import HeartIcon from '@/icons/home/heartIcon';
+import heroBg from '@/public/assets/home/hero-bg.png';
 
 export default function Hero() {
   return (
-    <section className="flex h-[80vh] flex-col items-center justify-center bg-[url('/assets/home/hero-bg.png')] bg-cover bg-no-repeat px-3 text-white">
-      <div className="w-full max-w-[800px] text-center">
+    <HeroLayout heroBg={heroBg}>
+      <div className="w-full max-w-[800px]">
         <Heading
           text="Empower Orphaned Children through Education and Hope"
           className="mb-4"
         />
         <Paragraph
           text="Your Compassionate Donation Can Make a Profound Difference: Join Us in Our Mission to Transform Lives and Give Hope to Children in Need of a Brighter Future"
-          className="mb-8"
+          className="mb-4"
         />
       </div>
       <Button
@@ -23,6 +25,6 @@ export default function Hero() {
         label="Donate Now"
         className="duration-200 hover:opacity-90"
       />
-    </section>
+    </HeroLayout>
   );
 }
