09f3cf161eb1afc9d9fc4bddc21a92500d82655b
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-26 23:38:00 +0100

    feat: add students json file

--- src/components/sponsor/sections/Children/index.tsx
+++ src/components/sponsor/sections/Children/index.tsx
@@ -1,139 +1,17 @@
 import React from 'react';
 
 import Wrapper from '@/components/layouts/Wrapper';
-import childImage from '@/public/assets/sponsor/child.png';
+import students from '@/lib/students.json';
 
 import ChildGrid from './core/ChildGrid';
 
-const CHILDREN = [
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-  {
-    src: childImage,
-    name: 'Hussain Ahmed',
-    dateOfBirth: '05/01/2007',
-    siblings: {
-      sisters: 3,
-      brothers: 3,
-    },
-    admiDate: '04/02/2018',
-  },
-];
 export default function Children() {
   return (
     <section className="pt-16">
       <Wrapper>
         <div className="grid place-items-center gap-y-28 md:grid-cols-2 md:gap-x-4 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-36 xl:grid-cols-4 xl:gap-x-44 xl:gap-y-36">
-          {CHILDREN.map((child, index: number) => (
-            <ChildGrid key={index} {...child} />
+          {students.map((student, index: number) => (
+            <ChildGrid key={index} {...student} />
           ))}
         </div>
       </Wrapper>
