34e360e58ce9785f53aa5b2549a3d2e63ab3670a
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-27 19:57:06 +0100

    feat: Video Iframe

--- src/components/gallery/sections/Galleries/Videos.tsx
+++ src/components/gallery/sections/Galleries/Videos.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function Videos() {
   return (
     <div className="flex h-[590px] w-full items-center justify-center rounded-2xl bg-white">
-      <p className="text-3xl font-medium text-green-1">Video</p>
+      <iframe
+        width="100%"
+        height="100%"
+        className="rounded-2xl"
+        src="https://player.vimeo.com/92c820de-dcd2-4e55-b653-12b17180a699"
+      ></iframe>
     </div>
   );
 }
