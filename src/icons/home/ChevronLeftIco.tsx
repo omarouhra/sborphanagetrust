a5e9b29cf3c86894ff86047a3bfdf3f92464f8c8
chaimaaSafi <safichaimaa098@gmail.com>
2023-11-27 19:57:42 +0100

    feat:  add Icons

---
+++ src/icons/home/ChevronLeftIco.tsx
@@ -0,0 +1,21 @@
+import React, { SVGProps } from 'react';
+
+export default function ChevronLeftIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-6 h-6"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+      />
+    </svg>
+  );
+}
